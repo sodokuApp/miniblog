53f316999438f7a430df82e2d8f4900f62823356
Susan <yaojianjing@126.com>
2018-06-11 01:11:42 +0800

    Our final release

--- project/models.py
+++
@@ -1,8 +0,0 @@
-from exts import db
-
-class User(db.Model):
-    __tablename__ = 'user'
-    id = db.Column(db.Integer,primary_key = True, autoincrement = True)
-    email = db.Column(db.String(100), nullable = False, unique = True)
-    username = db.Column(db.String(100), nullable = False)
-    password = db.Column(db.String(100), nullable = False)
